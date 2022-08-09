5cb4f4d696b323d81a5a4622e827c9049d9810d8
Nayara <nrdeandrade@gmail.com>
2022-08-09 16:18:58 -0300

    feat: estrutura do app, contexts

--- src/components/App.js
+++ src/components/App.js
@@ -1,3 +1,19 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { useState } from "react";
+import UserContext from '../context/UserContext';
+
+import Home from './layouts/Home';
+import SignIn from './layouts/SignIn';
+
 export default function App() {
-  return <>App</>;
+  return (
+    <BrowserRouter>
+      <UserContext.Provider>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/signin" element={<SignIn />} />
+        </Routes>
+      </UserContext.Provider>
+    </BrowserRouter>
+  )
 }
---
+++ src/components/layouts/SignIn.js
@@ -0,0 +1,7 @@
+export default function Signin() {
+    return (
+        <>
+            <h1>Signin</h1>
+        </>
+    )
+}
