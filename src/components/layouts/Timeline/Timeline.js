15a918edd4a0a918201944470a4369e1ea6fea25
pgeovany <pgeovany@ic.ufrj.br>
2022-08-11 21:18:31 -0300

    feat: implementa barra de pesquisa funcional (mas com problemas de layout)

--- src/components/layouts/Timeline/Timeline.js
+++ src/components/layouts/Timeline/Timeline.js
@@ -10,6 +10,7 @@ export default function Timeline() {
   const { token } = useContext(UserContext);
   const { image } = useContext(UserContext);
   const [activeMenu, setActiveMenu] = useState(false);
+  const [renderUserList, setRenderUserList] = useState(false);
 
   useEffect(() => {
     if (!token) {
@@ -20,14 +21,20 @@ export default function Timeline() {
   return (
     <>
       <Navbar
+        token={token}
         image={image}
         activeMenu={activeMenu}
         setActiveMenu={setActiveMenu}
+        renderUserList={renderUserList}
+        setRenderUserList={setRenderUserList}
       />
       <TimelineContainer
         onClick={() => {
           if (activeMenu) {
             setActiveMenu(false);
+          }
+          if (renderUserList) {
+            setRenderUserList(false);
           }
         }}
       >
